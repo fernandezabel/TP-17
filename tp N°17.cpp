e7334f830c4ab950c7ac7ec88b378d38c04c604a
fernandezabel <163901256+fernandezabel@users.noreply.github.com>
2024-05-06 14:30:12 -0300

    Add files via upload

---
+++ tp N°17.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int i = 0;
+	while (i<=10) 
+	{
+		i ++;
+    	if (i<10)
+    	{
+			i=0;
+		}
+		if (i==10)
+    	{
+			i=12;
+		}
+
+	}
+	
+	cout<<i<<endl;
+	return 0;
+}
+//se ejecuta 11 veces el cuerpo del programa
